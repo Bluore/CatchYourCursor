a59e7ee3e4a848af7266b5d7d6d28c28a4637d4c
Bluore <2260762800@qq.com>
2026-05-09 21:41:29 +0800

    实现多光标同步

--- front/index.js
+++ front/index.js
@@ -1,4 +1,5 @@
 const Host = window.location.host
+const CursorList = []
 
 var WSClient = new WebSocket(`ws://${Host}/api/cursor`)
 clientID = ""
@@ -8,7 +9,7 @@ WSClient.onmessage = function (event) {
     const data = JSON.parse(event.data)
     switch (data.status){
         case 1:
-            MoveTheCursor(data.data);
+            updateCursorList(data.data);
             break;
         case 6:
             UpdateClientID(data.data);
@@ -16,14 +17,41 @@ WSClient.onmessage = function (event) {
     }
 }
 
+WSClient.onopen = function (event) {
+}
+
 function UpdateClientID(data) {
     clientID = data.id
+
+    // 获取历史指针
+    const sendData = {
+        status: 5,
+        id: clientID,
+        data: {
+            id: clientID,
+            x: e.x,
+            y: e.y,
+        }
+    };
+    sendMsgToWSClient(sendData);
+}
+
+function CreateDOMCursor(data){
+    const parent = document.getElementById("cursor-list");
+    const child = document.createElement("div");
+
+    child.className = "cursor-item";
+    child.id = `cursor${data.id}`
+    child.style.top = data.x
+    child.style.left = data.y
+    
+    parent.appendChild(child)
 }
 
 function MoveTheCursor(data) {
     console.log(data)
     console.log(`move the cursor to (${data.x},${data.y})`)
-    const cursor = document.getElementById("test")
+    const cursor = document.getElementById(`cursor${data.id}`)
     cursor.style.left = `${data.x}px`
     cursor.style.top = `${data.y}px`
 }
@@ -31,7 +59,7 @@ function MoveTheCursor(data) {
 mouseMoveLastTime = 0
 document.addEventListener("mousemove",(e)=>{
     const mouseMoveNow = Date.now();
-    if (mouseMoveNow - mouseMoveLastTime < 100){
+    if (mouseMoveNow - mouseMoveLastTime < 50){
         return;
     }
     mouseMoveLastTime = mouseMoveNow;
@@ -53,4 +81,24 @@ async function sendMsgToWSClient(data){
     const sendMsg = JSON.stringify(data)
     WSClient.send(sendMsg)
     console.log(`already send msg: ${sendMsg}`)
+}
+
+function updateCursorList(data){
+    for (const item of CursorList){
+        if (item.id != data.id) continue;
+        item.x = data.x;
+        item.y = data.y;
+        MoveTheCursor(data);
+        return
+    }
+    CursorList.push(data)
+    CreateDOMCursor(data)
+}
+
+function reflashCursorList(dataList){
+    // todo reflash
+}
+
+function deleteCursorList(id){
+    // todo delete
 }
